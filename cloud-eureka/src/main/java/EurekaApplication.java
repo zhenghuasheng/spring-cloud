99ae879cd569650b0383ed4bc68a10fe28c24eae
zhenghuasheng <zhenghuasheng@aixuedai.com>
2017-10-20 11:07:16 +0800

    add eureka login config

--- cloud-eureka/src/main/java/EurekaApplication.java
+++ cloud-eureka/src/main/java/EurekaApplication.java
@@ -3,6 +3,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * Eureka默认端口是8761
+ * http://localhost:8761/eureka/apps 可以查看注册到该服务器上的一堆微服务实例的信息。
  * Created by zhenghuasheng on 2017/4/13.14:19
  */
 
